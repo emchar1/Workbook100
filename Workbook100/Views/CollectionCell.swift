48ee97b89635e842546bf2f7dc3d5431429d5750
Eddie Char <emchar1@gmail.com>
2022-02-07 18:47:42 -0800

    Add prepareForReuse() in CollectionCell to clear the imageView when it's scrolled off... genius! Start work on GloveCell to inherit CollectionCell.

--- Workbook100/Views/CollectionCell.swift
+++ Workbook100/Views/CollectionCell.swift
@@ -32,11 +32,6 @@ class CollectionCell: UICollectionViewCell {
         return noimg
     }()
     
-//    var ruleLine: RuleLine!
-//    var hStackBottom: CollectionCellStack!
-//    var labelSizesLeft: CollectionCellLabel!
-//    var labelSizesRight: CollectionCellLabel!
-
     override var isSelected: Bool {
         didSet {
             setSelected(isSelected, in: contentView)
@@ -56,12 +51,12 @@ class CollectionCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented.")
     }
     
-//    override func prepareForReuse() {
-//        super.prepareForReuse()
-//
-//        productImage.image = nil
-//        productImage.cancelImageLoad()
-//    }
+    override func prepareForReuse() {
+        super.prepareForReuse()
+
+        productImage.image = nil
+        productImage.cancelImageLoad()
+    }
     
     func setViews() {
         //New & Essential Stack
@@ -71,21 +66,8 @@ class CollectionCell: UICollectionViewCell {
 
         //Product Title
         labelTitle.text = model.productNameDescription
-        labelSubtitle.text = model.productNameDescriptionSecondary + "\n" + model.colorway
+        labelSubtitle.text = model.productNameDescriptionSecondary
         
-//
-//        if contentView.frame.width >= 200 {
-//            hStackBottom.isHidden = false
-//            ruleLine.isHidden = false
-//        }
-//        else {
-//            hStackBottom.isHidden = true
-//            ruleLine.isHidden = true
-//        }
-//
-//        labelSizesLeft.text = layoutSizes().left
-//        labelSizesRight.text = layoutSizes().right
-//
         //OLD WAY Uses images saved in Firebase Storage
 //        if let image = model.image {
 //            image.downloadURL { (url, error) in
@@ -109,12 +91,12 @@ class CollectionCell: UICollectionViewCell {
     
     // MARK: - Helper Functions
     
-    private func setupViews() {
+    func setupViews() {
         self.model = CollectionModel.getBlankModel()
         
         //==SETUP==//
         //Content View
-//        contentView.backgroundColor = .white
+//        contentView.backgroundColor = .magenta
 //        contentView.layer.cornerRadius = K.CollectionCell.cornerRadius
 //        contentView.clipsToBounds = true
 
@@ -129,19 +111,13 @@ class CollectionCell: UICollectionViewCell {
         
         //Product Title
         labelTitle = CollectionCellLabel(type: .title, text: model.productNameDescription)
-        labelSubtitle = CollectionCellLabel(type: .subtitle, text: model.colorway)
+        labelSubtitle = CollectionCellLabel(type: .subtitle, text: model.productNameDescriptionSecondary)
         
         //Product Image
         productImage = UIImageView()
-//        ruleLine = RuleLine(frame: CGRect(x: 0, y: 0, width: K.CollectionCell.width, height: 20))
-//        hStackBottom = CollectionCellStack(distribution: .fillEqually, alignment: .fill, axis: .horizontal)
-//        labelSizesLeft =  CollectionCellLabel(type: .productSize, text: layoutSizes().left)
-//        labelSizesRight =  CollectionCellLabel(type: .productSize, text: layoutSizes().right)
-        
-//        let padding: CGFloat = K.CollectionCell.padding
-
         
 
+        
         //==SUBVIEWS==//
         //VStack
         contentView.addSubview(vStack)
@@ -170,16 +146,6 @@ class CollectionCell: UICollectionViewCell {
         productImage.addSubview(productImageNoImg)
         NSLayoutConstraint.activate([productImageNoImg.centerXAnchor.constraint(equalTo: productImage.centerXAnchor),
                                      productImageNoImg.centerYAnchor.constraint(equalTo: productImage.centerYAnchor)])
-
-        //Rule Line
-//        vStack.addArrangedSubview(ruleLine)
-        
-        // FIXME: - Sizes
-//        vStack.addArrangedSubview(hStackBottom)
-//        hStackBottom.addArrangedSubview(labelSizesLeft)
-//        hStackBottom.alignment = .top
-//        labelSizesRight.textAlignment = .right
-//        hStackBottom.addArrangedSubview(labelSizesRight)
     }
         
     private func startSpinner(in view: UIView) {
@@ -195,44 +161,4 @@ class CollectionCell: UICollectionViewCell {
             self.spinner.stopAnimating()
         }
     }
-    
-    
-//    private func layoutSizes() -> (left: String, right: String) {
-//        var leftReturn = ""
-//        var rightReturn = ""
-//        var sizesCount = 0
-//        var sizesCountHalved: Int {
-//            Int(ceil(Double(sizesCount) / 2.0))
-//        }
-//        var maxSizesCountHalved: Int {
-//            Int(ceil(Double(model.sizes.count) / 2.0))
-//        }
-//
-//        //Get the number of viable sizes
-//        for (i, size) in model.sizes.enumerated() {
-//            if size.size == "" && size.colorwaySKU == "" {
-//                sizesCount = i
-//                break
-//            }
-//        }
-//
-//        //Populate left side
-//        for i in 0..<sizesCountHalved {
-//            leftReturn += "\(model.sizes[i])"
-//            leftReturn += (i < sizesCountHalved - 1) ? "\n" : ""
-//        }
-//
-//        //Populate right side
-//        for i in sizesCountHalved..<sizesCount {
-//            rightReturn += "\(model.sizes[i])"
-//            rightReturn += (i < sizesCount - 1) ? "\n" : ""
-//        }
-//
-//        //Add "padding"
-//        for _ in sizesCountHalved..<maxSizesCountHalved {
-//            leftReturn += "\n"
-//        }
-//
-//        return (leftReturn, rightReturn)
-//    }
 }
--- Workbook100/Views/ProductCategories/GloveCell.swift
+++ Workbook100/Views/ProductCategories/GloveCell.swift
@@ -8,7 +8,84 @@
 import UIKit
 
 class GloveCell: CollectionCell {
+    var ruleLine: RuleLine!
+    var hStackBottom: CollectionCellStack!
+    var labelSizesLeft: CollectionCellLabel!
+    var labelSizesRight: CollectionCellLabel!
+    
+    override func setViews() {
+        super.setViews()
 
+//        if contentView.frame.width >= 200 {
+//            hStackBottom.isHidden = false
+//            ruleLine.isHidden = false
+//        }
+//        else {
+//            hStackBottom.isHidden = true
+//            ruleLine.isHidden = true
+//        }
+        
+        labelSizesLeft.text = layoutSizes().left
+        labelSizesRight.text = layoutSizes().right
+    }
+    
+    override func setupViews() {
+        super.setupViews()
+        
+        ruleLine = RuleLine(frame: CGRect(x: 0, y: 0, width: K.CollectionCell.width, height: 20))
+        hStackBottom = CollectionCellStack(distribution: .fillEqually, alignment: .fill, axis: .horizontal)
+        labelSizesLeft =  CollectionCellLabel(type: .productSize, text: layoutSizes().left)
+        labelSizesRight =  CollectionCellLabel(type: .productSize, text: layoutSizes().right)
+
+        //Rule Line
+        vStack.addArrangedSubview(ruleLine)
+
+        // FIXME: - Sizes
+        vStack.addArrangedSubview(hStackBottom)
+        hStackBottom.addArrangedSubview(labelSizesLeft)
+        hStackBottom.alignment = .top
+        labelSizesRight.textAlignment = .right
+        hStackBottom.addArrangedSubview(labelSizesRight)
+    }
+    
+    private func layoutSizes() -> (left: String, right: String) {
+        var leftReturn = ""
+        var rightReturn = ""
+        var sizesCount = 0
+        var sizesCountHalved: Int {
+            Int(ceil(Double(sizesCount) / 2.0))
+        }
+        var maxSizesCountHalved: Int {
+            Int(ceil(Double(model.sizes.count) / 2.0))
+        }
+        
+        //Get the number of viable sizes
+        for (i, size) in model.sizes.enumerated() {
+            if size.size == "" && size.colorwaySKU == "" {
+                sizesCount = i
+                break
+            }
+        }
+        
+        //Populate left side
+        for i in 0..<sizesCountHalved {
+            leftReturn += "\(model.sizes[i])"
+            leftReturn += (i < sizesCountHalved - 1) ? "\n" : ""
+        }
+        
+        //Populate right side
+        for i in sizesCountHalved..<sizesCount {
+            rightReturn += "\(model.sizes[i])"
+            rightReturn += (i < sizesCount - 1) ? "\n" : ""
+        }
+        
+        //Add "padding"
+        for _ in sizesCountHalved..<maxSizesCountHalved {
+            leftReturn += "\n"
+        }
+        
+        return (leftReturn, rightReturn)
+    }
 }
 
 
