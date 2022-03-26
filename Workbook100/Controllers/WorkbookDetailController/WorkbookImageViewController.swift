3663f74c276277975a8bff3346f434661d8027f3
Eddie Char <emchar1@gmail.com>
2022-03-26 13:47:07 -0700

    Update WorkbookImageViewController: changed IBOutlet to simple vars. Update constraints. Add double-tap to zoom.

--- Workbook100/Controllers/WorkbookDetailController/WorkbookImageViewController.swift
+++ Workbook100/Controllers/WorkbookDetailController/WorkbookImageViewController.swift
@@ -8,54 +8,57 @@
 import UIKit
 
 class WorkbookImageViewController: UIViewController, UIScrollViewDelegate {
-    @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var imageView: UIImageView!
+    var scrollView: UIScrollView!
+    var imageView: UIImageView!
     
-    var imageViewTop: NSLayoutConstraint!
-    var imageViewLeading: NSLayoutConstraint!
-    var imageViewTrailing: NSLayoutConstraint!
-    var imageViewBottom: NSLayoutConstraint!
-
     var spinner = ActivitySpinner()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Set up views
+        scrollView = UIScrollView()
         scrollView.delegate = self
         scrollView.minimumZoomScale = 1.0
         scrollView.maximumZoomScale = 12.0
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(scrollView)
         
+        imageView = UIImageView()
         imageView.contentMode = .scaleAspectFit
-//        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageViewTop = imageView.topAnchor.constraint(equalTo: scrollView.topAnchor)
-        imageViewLeading = imageView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor)
-        imageViewTrailing = scrollView.trailingAnchor.constraint(equalTo: imageView.trailingAnchor)
-        imageViewBottom = scrollView.bottomAnchor.constraint(equalTo: imageView.bottomAnchor)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.addSubview(imageView)
         
-//        addImageConstraints()
-
+        //Set up constraints
+        NSLayoutConstraint.activate([scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+                                     scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                                     view.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+                                     view.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor)])
+        
+        NSLayoutConstraint.activate([imageView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+                                     imageView.heightAnchor.constraint(equalTo: scrollView.heightAnchor),
+                                     imageView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+                                     imageView.centerYAnchor.constraint(equalTo: scrollView.centerYAnchor)])
+        
+        
+        //Double-tap to zoom
+        let doubleTapRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleDoubleTap(_:)))
+        doubleTapRecognizer.numberOfTapsRequired = 2
+        view.addGestureRecognizer(doubleTapRecognizer)
     }
     
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-//        if scrollView.zoomScale == scrollView.minimumZoomScale {
-//            addImageConstraints()
-//        }
-//        else {
-//            removeImageConstraints()
-//        }
         return imageView
     }
     
-    func addImageConstraints() {
-        print("adding constraints")
-        NSLayoutConstraint.activate([imageViewTop, imageViewLeading, imageViewTrailing, imageViewBottom])
+    @objc func handleDoubleTap(_ sender: UITapGestureRecognizer) {
+        if scrollView.zoomScale == scrollView.minimumZoomScale {
+            scrollView.setZoomScale(scrollView.maximumZoomScale / 4, animated: true)
+        }
+        else {
+            scrollView.setZoomScale(scrollView.minimumZoomScale, animated: true)
+        }
     }
-    
-    func removeImageConstraints() {
-        print("removing constraints")
-        NSLayoutConstraint.deactivate([imageViewTop, imageViewLeading, imageViewTrailing, imageViewBottom])
-    }
-    
     
     @IBAction func doneTapped(_ sender: UIBarButtonItem) {
         dismiss(animated: true)
