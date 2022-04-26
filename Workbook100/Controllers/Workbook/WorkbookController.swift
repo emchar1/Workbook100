97e3410fe3080af1292e62fc0e6196061ecf1509
Eddie Char <emchar1@gmail.com>
2022-04-25 20:12:35 -0700

    Refactor.

---
+++ Workbook100/Controllers/Workbook/WorkbookController.swift
@@ -0,0 +1,18 @@
+//
+//  WorkbookController.swift
+//  Workbook100
+//
+//  Created by Eddie Char on 4/25/22.
+//
+
+import UIKit
+
+class WorkbookController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .magenta
+    }
+}
