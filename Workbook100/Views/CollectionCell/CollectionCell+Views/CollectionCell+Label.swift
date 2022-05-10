d2df2821a3cfe14f0547f750f35060110ca7a459
Eddie Char <emchar1@gmail.com>
2022-05-10 12:06:59 -0700

    Break out CollectionCell+Views to separate files.

---
+++ Workbook100/Views/CollectionCell/CollectionCell+Views/CollectionCell+Label.swift
@@ -0,0 +1,44 @@
+//
+//  CollectionCell+Label.swift
+//  Workbook100
+//
+//  Created by Eddie Char on 5/10/22.
+//
+
+import UIKit
+import Darwin
+
+class CollectionCellLabel: UILabel {
+    enum LabelType {
+        case title, subtitle, productSize
+    }
+    
+    init(frame: CGRect = .zero, type: LabelType, text: String) {
+        super.init(frame: frame)
+
+        self.text = text
+//        self.textColor = .black
+        self.numberOfLines = (type != .title) ? 0 : 1
+        
+        if frame == .zero {
+            self.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        customizeType(type)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func customizeType(_ type: LabelType) {
+        switch type {
+        case .title:
+            self.font = .workbookTitle
+        case .subtitle:
+            self.font = .workbookSubtitle
+        case .productSize:
+            self.font = .workbookFooterTitle
+        }
+    }
+}
---
+++ Workbook100/Views/CollectionCell/CollectionCell+Views/CollectionCell+LabelBubble.swift
@@ -0,0 +1,49 @@
+//
+//  CollectionCell+LabelBubble.swift
+//  Workbook100
+//
+//  Created by Eddie Char on 5/10/22.
+//
+
+import UIKit
+
+class CollectionCellLabelBubble: UILabel {
+    enum LabelBubbleType {
+        case new, essential, nothing
+    }
+    
+    init(frame: CGRect = .zero, type: LabelBubbleType) {
+        super.init(frame: frame)
+
+        self.text = text
+        self.textColor = type == .essential ? .systemBackground : .white
+        self.textAlignment = .center
+        self.font = .workbookBubbleTitle
+        self.layer.cornerRadius = 8
+        self.clipsToBounds = true
+        
+        if frame == .zero {
+            self.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        customizeType(type)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func customizeType(_ type: LabelBubbleType) {
+        switch type {
+        case .new:
+            self.backgroundColor = .red
+            self.text = "New"
+        case .essential:
+            self.backgroundColor = .label
+            self.text = "Essential"
+        case .nothing:
+            self.backgroundColor = .clear
+            self.text = ""
+        }
+    }
+}
